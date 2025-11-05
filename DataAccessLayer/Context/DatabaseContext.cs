bfefda5e0c9976358ec7236c6806212363a0b6fa
jonasgregersen <eaa24jbg@students.eaaa.dk>
2025-11-05 10:57:39 +0100

    Repository

--- DataAccessLayer/Context/DatabaseContext.cs
+++ DataAccessLayer/Context/DatabaseContext.cs
@@ -6,11 +6,6 @@ namespace DataAccessLayer.Context;
 public class DatabaseContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Product>().ToTable("Product");
-    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=tcp:10.10.132.118,1433;Database=NaturePlanet;User Id=sa;Password=12345;TrustServerCertificate=True;");
