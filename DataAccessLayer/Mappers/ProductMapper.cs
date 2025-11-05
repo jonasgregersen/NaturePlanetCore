6fe082221f350937cb2ad646c6c4e29202b735ef
danielroschmann <eaa24daro@students.eaaa.dk>
2025-11-05 11:41:04 +0100

    Mapper implemntation 2

--- DataAccessLayer/Mappers/ProductMapper.cs
+++ DataAccessLayer/Mappers/ProductMapper.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTOProduct = DataTransferLayer.Model.Product;
+using DALProduct = DataAccessLayer.Model.Product;
 
 namespace DataAccessLayer.Mappers
 {
-    internal class ProductMapper
+    public class ProductMapper
     {
+
+        public static DTOProduct Map(DALProduct product)
+        {
+            if (product != null)
+            {
+                return new DTOProduct(product.Name, product.EAN, product.ErpSource, product.Active, product.QuantityInBag,
+                                      product.Weight, product.Segment, product.ProductCategory1, product.ProductCategory2,
+                                      product.ProductCategory3);
+            }
+            return null;
+        }
+
+        public static DALProduct Map(DTOProduct product)
+        {
+            if (product != null)
+            {
+                return new DALProduct(product.Name, product.EAN, product.ErpSource, product.Active, product.QuantityInBag,
+                                      product.Weight, product.Segment, product.ProductCategory1, product.ProductCategory2,
+                                      product.ProductCategory3);
+            }
+            return null;
+        }
     }
 }
