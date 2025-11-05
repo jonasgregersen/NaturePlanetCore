12913cc357f5a933574810087e1033ae97b6f23e
danielroschmann <eaa24daro@students.eaaa.dk>
2025-11-05 11:29:52 +0100

    .

--- DataAccessLayer/Model/Product.cs
+++ DataAccessLayer/Model/Product.cs
@@ -13,4 +13,21 @@ public class Product
     public string ProductCategory1 { get; set; }
     public string ProductCategory2 { get; set; }
     public string ProductCategory3 { get; set; }
+
+    public Product(string name, int ean, string erpSource, bool active, int quantityInBag, double weight,
+        string segment, string productCategory1, string productCategory2, string productCategory3)
+    {
+        Name = name;
+        EAN = ean;
+        ErpSource = erpSource;
+        Active = active;
+        QuantityInBag = quantityInBag;
+        Weight = weight;
+        Segment = segment;
+        ProductCategory1 = productCategory1;
+        ProductCategory2 = productCategory2;
+        ProductCategory3 = productCategory3;
+    }
+
+    
 }
