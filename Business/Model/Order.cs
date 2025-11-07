6e8456b8a8eafff792af9b7c04e9c25ec436c23d
Asger <eaaashr@students.eaaa.dk>
2025-11-07 08:57:08 +0100

    Order + User

---
+++ Business/Model/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Model;
+
+namespace Business.Model
+{
+    public class Order
+    {
+        public int OrderNumber { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public double GetTotalPrice()
+        {
+            double total = 0;
+            foreach (Product product in Products)
+            {
+                total += product.getPrice();
+            }
+            return total;
+        }
+
+        public List<Product> GetProducts()
+        {
+            return new List<Product>(Products);
+        }
+    }
+}
---
+++ Business/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Business.Model
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public int authenticationLevel { get; set; }
+        public List<Order> orderHistory { get; set; } = new List<Order>();
+    }
+}
