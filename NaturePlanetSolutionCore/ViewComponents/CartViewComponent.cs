dbe6b76590884101e611320d6718ab1c8aa770a1
jonasgregersen <eaa24jbg@students.eaaa.dk>
2025-11-18 10:13:39 +0100

    cart

--- NaturePlanetSolutionCore/ViewComponents/CartViewComponent.cs
+++ NaturePlanetSolutionCore/ViewComponents/CartViewComponent.cs
@@ -1,6 +1,20 @@
+using DataAccessLayer.Model;
+using DataTransferLayer.Model;
+using Microsoft.AspNetCore.Mvc;
+
 namespace NaturePlanetSolutionCore.ViewComponents;
 
-public class CartViewComponent
+public class CartViewComponent : ViewComponent
 {
-    
+    public IViewComponentResult Invoke()
+    {
+        var cart = HttpContext.Session.GetObject<OrderDto>("order");
+        if (cart == null)
+        {
+            cart = new OrderDto();
+            cart.Products = new List<ProductDto>();
+        }
+
+        return View(cart);
+    }
 }
