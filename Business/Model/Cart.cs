c552ec3700a78337d38cb397703359f212648a97
jonasgregersen <eaa24jbg@students.eaaa.dk>
2025-11-21 16:29:39 +0100

    Confirmation side

--- Business/Model/Cart.cs
+++ Business/Model/Cart.cs
@@ -22,4 +22,15 @@ public class Cart
     {
         return RandomNumberGenerator.GetInt32(10000);
     }
+
+    public int GetProductQuantity(ProductDto product)
+    {
+        return Products.Where(p => p.Id == product.Id).Count();
+    }
+
+    
+    public List<ProductDto> GetProductWithoutDuplicates()
+    {
+        return Products.Distinct().GroupBy(p => p.Name).Select(g => g.First()).ToList();
+    }
 }
